4fedce69e11252a01381f5978e6fdfd46c4053a6
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-07 17:40:51 +0100

    hash werkt

--- eindopdracht/modules/API.js
+++ eindopdracht/modules/API.js
@@ -28,15 +28,27 @@ export function getData() {
 
 }
 
+let id = 0;
+
 export function renderData(quotes){
     quotes.data.forEach(data => {
-    list.insertAdjacentHTML('afterbegin', 
-    `<li>
+    list.insertAdjacentHTML('beforeend', 
+    `<li id=${id = id + 1}>
         <img src='${data.avatar}' alt='profielfoto' onerror="this.style.display='none'" >
         <h2>${data.name}</h2>
         <p>${data.text}</p>
     </li>`)
  })
 }
- 
 
+
+// setTimeout(() => {window.location.hash = '#1';}, 1000)
+window.addEventListener('scroll', function() {
+    const liEl = document.querySelectorAll('ul li');
+    for(let i = 0; i < liEl.length; i++) {
+        let rect = liEl[i].getBoundingClientRect();
+        if(rect.bottom > 0 && rect.bottom < 550) {
+            window.location.hash = liEl[i].id;
+        }
+    } 
+})
