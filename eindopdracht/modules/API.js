aad6a6a616591434fd7681f4074a5bdac50f508c
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-08 14:32:34 +0100

    button werkt

--- eindopdracht/modules/API.js
+++ eindopdracht/modules/API.js
@@ -40,17 +40,56 @@ export function renderData(quotes){
     </li>`)
  })
 }
+;
+
+const quotes = [
+    '#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10',
+]
+
+button.onclick = function() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+    var base_url = window.location.origin;
+
+    window.location.href = base_url +  randomQuote
+}
+
+const hallo = document.querySelector("#hallo")
+function randomQuotes(quotes) {
+    hallo.insertAdjacentHTML('beforeend', 
+    `<li id=${id = 2}>
+        <img src='${quotes.avatar}' alt='profielfoto' onerror="this.style.display='none'" >
+        <h2>${quotes.name}</h2>
+        <p>${quotes.text}</p>
+    </li>`)
+}
+
+// var button = document.getElementById("button");
+// var myarray = ["li"];
+
+// button.onclick = function() {
+//     alert(myarray[Math.floor(Math.random() * myarray.length)]);
+// };
+
+
+// document.getElementById('button');
+
+// //;addEventListener("click", myFunction());
+
+// function myFunction(randomQuotes) {
+//     return randomQuotes = list [Math.floor(Math.random()*list.length)];
+// }
 
 
 // setTimeout(() => {window.location.hash = '#1';}, 1000)
-const liEl = document.querySelectorAll('ul li');
-for(let i = 0; i < liEl.length; i++) {
-    liEl[i].addEventListener('mouseover', function() {
-        console.log('hoi')
-        let rect = liEl[i].getBoundingClientRect();
-        if(rect.bottom > 0 && rect.bottom < 550) {
-            window.location.hash = liEl[i].id;
-        }
+// const liEl = document.querySelectorAll('ul li');
+// for(let i = 0; i < liEl.length; i++) {
+//     liEl[i].addEventListener('mouseenter', function() {
+//         console.log('hoi')
+//         let rect = liEl[i].getBoundingClientRect();
+//         if(rect.bottom > 0 && rect.bottom < 550) {
+//             window.location.hash = liEl[i].id;
+//         }
     
-    })
-} 
+//     })
+// } 
